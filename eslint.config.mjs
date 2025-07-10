318cc25842ea456c8946afe5a2f3d54dd1a359e7
phamquyetthang <quyetthang.phucchi@gmail.com>
2025-07-10 21:31:58 +0700

    Add demo page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,11 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
-const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
@@ -19,13 +20,26 @@ const compat = new FlatCompat({
 export default defineConfig([
   includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+          alwaysTryTypes: true,
+        },
+      },
+    },
+    // plugins: {
+    //   import: require('eslint-plugin-import'),
+    // },
+    rules: {
+      'import/no-unresolved': 'error',
+    },
     extends: fixupConfigRules(
       compat.extends(
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:import/recommended',
-        'plugin:import/electron',
         'plugin:import/typescript'
       )
     ),
@@ -35,6 +49,9 @@ export default defineConfig([
         ...globals.node,
       },
       parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
     },
   },
   globalIgnores(['!.husky/*', 'eslint.config.mjs']),
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
-import Test from "@/app/components/test";
-import { Button } from "./components/ui/button";
+import { LoginForm } from './components/login-form';
 
 const App = () => {
   return (
-    <div>
-      <Test />
-      <Button >button</Button>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
     </div>
   );
 };
