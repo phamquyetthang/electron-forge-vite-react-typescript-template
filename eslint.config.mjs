b92f9fc01cebfcbdd41212d9b3e0c4cf43dc8abf
phamquyetthang <quyetthang.phucchi@gmail.com>
2025-07-10 21:05:30 +0700

    add git hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,41 @@
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { fixupConfigRules, includeIgnoreFile } from '@eslint/compat';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default defineConfig([
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:import/recommended",
-      "plugin:import/electron",
-      "plugin:import/typescript",
-    ],
-  },
-  tseslint.configs.recommended,
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "error",
+    extends: fixupConfigRules(
+      compat.extends(
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/recommended',
+        'plugin:import/electron',
+        'plugin:import/typescript'
+      )
+    ),
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tsParser,
     },
   },
+  globalIgnores(['!.husky/*', 'eslint.config.mjs']),
 ]);
